e2202d6381da341ad15b13b2f6edbaec1e22cccb
PrathamRawat <prathamrawat815@gmail.com>
2020-06-22 15:56:37 -0400

    Add scripts

---
+++ setup_tezos.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# Delete old tezos repository
+if [ -d "tezos" ]; then
+  echo "Removing Tezos"
+  rm -rf tezos
+  echo "Removed"
+fi
+
+# Clone repository and initialize opam
+git clone https://gitlab.com/tezos/tezos.git
+cd tezos
+git checkout latest-release
+opam init --bare
+
+# Create compiler version for ocaml
+opam switch create tezos 4.09.1
+eval $(opam env)
+
+# Get binaries
+opam install depext
+opam depext tezos
+
+# Install binaries
+opam install tezos
