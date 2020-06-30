52ac8e4dd6175803cc2fa3a4c1bc072911168e54
PrathamRawat <prathamrawat815@gmail.com>
2020-06-30 10:41:05 -0400

    Fix conseil script to setup database

--- app/util/scripts/setup_conseil.sh
+++ app/util/scripts/setup_conseil.sh
@@ -2,13 +2,9 @@
 
 docker pull cryptonomictech/conseil:latest
 
-sudo -i -u postgres
-
-createdb "conseil-$1" --username="user"
+psql -c "CREATE DATABASE conseil-$1"
 
 psql "conseil-$1" -f data/conseil.sql
-
-exit
 
 #docker run -e XTZ_Host=localhost -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_Database="conseil-$1" "conseil-$1" conseil-lorre
 
