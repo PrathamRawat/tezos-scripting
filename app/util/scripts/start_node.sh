4f2bb467dee44631ffae111d6d18764180313521
PrathamRawat <prathamrawat815@gmail.com>
2020-06-29 20:41:28 -0400

    Fix node restart code

--- app/util/scripts/start_node.sh
+++ app/util/scripts/start_node.sh
@@ -12,4 +12,4 @@ cd util/tezos
 
 screen -dm -S "$4" ./tezos-node run --data-dir ../tezos-nodes/data/"$4" --config-file ../tezos-nodes/config/"$4".json --network "$1" --rpc-addr 127.0.0.1:"$2" --net-addr 127.0.0.1:"$3"
 
-export node_pid=$(screen -ls | awk '/\."$4"\t/ {print strtonum($1)}')
+#export node_pid=$(screen -ls | awk '/\."$4"\t/ {print strtonum($1)}')
