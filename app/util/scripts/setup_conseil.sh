d921d81efd4d71446cdcf172a7542fc23bed1c10
PrathamRawat <prathamrawat815@gmail.com>
2020-07-01 12:53:27 -0400

    Add ability to login to conseil role for postgres

--- app/util/scripts/setup_conseil.sh
+++ app/util/scripts/setup_conseil.sh
@@ -7,7 +7,7 @@ createdb "conseil_$1"
 psql "conseil_$1" -f util/data/conseil.sql
 
 # Create postgres user for conseil
-psql -d "conseil_$1" -U $(whoami) -c "CREATE ROLE conseil WITH PASSWORD 'conseil';"
+psql -d "conseil_$1" -U $(whoami) -c "CREATE ROLE conseil WITH LOGIN PASSWORD 'conseil';"
 
 #docker run -e XTZ_Host=localhost -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_Database="conseil-$1" "conseil-$1" conseil-lorre
 
