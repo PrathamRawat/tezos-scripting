a24bef33f99b1a0b1e63ee71ce792c01b40b7907
PrathamRawat <prathamrawat815@gmail.com>
2020-07-01 12:16:08 -0400

    Add default database port to conseil start

--- app/util/scripts/run_conseil.sh
+++ app/util/scripts/run_conseil.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -e XTZ_Host="localhost" -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_User="conseil" -e DB_Password="conseil" -e DB_Database="conseil_$1" -e DB_Host="localhost" --name "conseil-lorre-$1" "cryptonomictech/conseil:latest" "lorre"
+docker run -e XTZ_Host="localhost" -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_User="conseil" -e DB_Password="conseil" -e DB_Database="conseil_$1" -e DB_Port="5432" -e DB_Host="localhost" --name "conseil-lorre-$1" "cryptonomictech/conseil:latest" "lorre"
 
-docker run -e XTZ_Host="localhost" -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_User="conseil" -e DB_Password="conseil" -e DB_Database="conseil_$1" -e DB_Host="localhost" --name "conseil-api-$1" "cryptonomictech/conseil:latest" "conseil"
+docker run -e XTZ_Host="localhost" -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_User="conseil" -e DB_Password="conseil" -e DB_Database="conseil_$1" -e DB_Port="5432" -e DB_Host="localhost" --name "conseil-api-$1" "cryptonomictech/conseil:latest" "conseil"
