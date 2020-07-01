3a92717d6db466a7bbf2fddeadd55afe64ea4106
PrathamRawat <prathamrawat815@gmail.com>
2020-07-01 09:51:31 -0400

    Update run conseil with proper entrypoints

--- app/util/scripts/run_conseil.sh
+++ app/util/scripts/run_conseil.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -e XTZ_Host=localhost -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_Database="conseil_$1" --name "conseil-lorre-$1" "cryptonomictech/conseil" "conseil-lorre"
+docker run -e XTZ_Host=localhost -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_Database="conseil_$1" --name "conseil-lorre-$1" "cryptonomictech/conseil" "lorre"
 
-docker run -e XTZ_Host=localhost -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_Database="conseil_$1" --name "conseil-api-$1" "cryptonomictech/conseil" "conseil-api"
+docker run -e XTZ_Host=localhost -e XTZ_Port="$2" -e XTZ_Network="$3" -e API_PORT="$4" -e DB_Database="conseil_$1" --name "conseil-api-$1" "cryptonomictech/conseil" "conseil"
