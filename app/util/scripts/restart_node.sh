a43cc453b358f2afac2a817b5616c28bf44deb74
PrathamRawat <prathamrawat815@gmail.com>
2020-07-13 14:58:18 -0400

    Fix restarting tezos node, add snapshot start functionality

--- app/util/scripts/restart_node.sh
+++ app/util/scripts/restart_node.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd util/tezos
+docker start "tezos-node-$4"
 
-./tezos-node identity --data-dir ../tezos-nodes/data/"$4" --config-file ../tezos-nodes/config/"$4".json generate
+#cd util/tezos
 
-screen -dm -S "$4" ./tezos-node run --data-dir ../tezos-nodes/data/"$4" --config-file ../tezos-nodes/config/"$4".json --network "$1" --rpc-addr 127.0.0.1:"$2" --net-addr 127.0.0.1:"$3" --history-mode "$5"
+#./tezos-node identity --data-dir ../tezos-nodes/data/"$4" --config-file ../tezos-nodes/config/"$4".json generate
+
+#screen -dm -S "$4" ./tezos-node run --data-dir ../tezos-nodes/data/"$4" --config-file ../tezos-nodes/config/"$4".json --network "$1" --rpc-addr 127.0.0.1:"$2" --net-addr 127.0.0.1:"$3" --history-mode "$5"
