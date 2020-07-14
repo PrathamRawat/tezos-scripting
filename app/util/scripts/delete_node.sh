8c92315e1baaa95adead6d04a960f8d2cb931477
PrathamRawat <prathamrawat815@gmail.com>
2020-07-14 11:13:13 -0400

    Delete postgres container when deleting node

--- app/util/scripts/delete_node.sh
+++ app/util/scripts/delete_node.sh
@@ -4,11 +4,11 @@ docker rm "conseil-api-$1"
 
 docker rm "conseil-lorre-$1"
 
+docker rm "conseil-postgres-$1"
+
 docker rm "arronax-$1"
 
 docker rm "tezos-node-$1"
-
-dropdb "conseil_$1"
 
 rm -rf "util/tezos-nodes/data/$1"
 
